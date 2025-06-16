403a131d1f6e20f48167b19cc7c77446b76190eb
Reemap7575 <rpanchal7575@conestogac.on.ca>
2025-06-15 23:56:59 -0400

    Update calculateCanvasSize.ts

--- SENG8120-Modern-Automated-Testing-evaluating-for-test-coverage/src/calculateCanvasSize/calculateCanvasSize.ts
+++ SENG8120-Modern-Automated-Testing-evaluating-for-test-coverage/src/calculateCanvasSize/calculateCanvasSize.ts
@@ -1,6 +1,5 @@
-export default function calculateCanvasSize(
-  length: string,
-  width: string
-) {
-  return parseInt(length) * parseInt(width);
+
+export function calculateCanvasSize(width: number, height: number): number {
+  if (width <= 0 || height <= 0) throw new Error("Invalid dimensions");
+  return width * height;
 }
