41c7af4204b1c4956b514941a0a6872388ccd1e6
Reemap7575 <rpanchal7575@conestogac.on.ca>
2025-06-15 23:57:47 -0400

    Update paintRequiredCalculator.ts

--- SENG8120-Modern-Automated-Testing-evaluating-for-test-coverage/src/paintRequiredCalculator/paintRequiredCalculator.ts
+++ SENG8120-Modern-Automated-Testing-evaluating-for-test-coverage/src/paintRequiredCalculator/paintRequiredCalculator.ts
@@ -1,3 +1,6 @@
-export default function paintRequiredCalculator(area: number, coveragePerLiter: number): number {
-  return area / coveragePerLiter;
+
+export function paintRequired(area: number, coveragePerLitre: number): number {
+  if (area <= 0 || coveragePerLitre <= 0) throw new Error("Invalid input");
+  return Math.ceil(area / coveragePerLitre);
 }
+
